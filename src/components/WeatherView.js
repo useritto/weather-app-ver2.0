7669fb5191551c247213551827c1a28d14ec178e
Evgeny Vlasov <e.vlasov@foxford.ru>
2022-03-23 02:14:47 +0300

    Improve weather day card component

--- src/components/WeatherView.js
+++ src/components/WeatherView.js
@@ -26,11 +26,11 @@ class WeatherView extends React.Component {
                     city={this.cityName()}
                 />
                 <Box display="flex">
-                    <Grid container spacing={1}>
+                    <div style={{display: 'flex', gap: '20px', justifyContent: 'center', width: '100%'}}>
                         {this.props.weather.daily.map(day =>
                             <AdditionalCard weather={prepareWeatherData(day, this.props.weather.timezone)} />
                         )}
-                    </Grid>
+                    </div>
                 </Box>
             </Container>
         );
